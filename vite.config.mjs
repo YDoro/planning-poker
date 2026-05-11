65dc7eff59ffd6b8a0c5057ed497722adb965a99
yan doro <yan_mvd@hotmail.com>
2026-05-11 19:20:48 -0300

    feat: add shadcnui for now

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,4 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import EnvironmentPlugin from 'vite-plugin-environment';
@@ -8,7 +9,7 @@ delete process.env['ProgramFiles(x86)'];
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), tailwindcss(), EnvironmentPlugin('all')],
+  plugins: [react(), tailwindcss(), EnvironmentPlugin('all'), tsconfigPaths()],
   server: {
     host: true,
   },
