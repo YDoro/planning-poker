44961aa24f48950ea5208bfedf77b97a176996a9
yan doro <yan_mvd@hotmail.com>
2026-05-13 12:06:28 -0300

    refactor(home): split homepage into multiple components

---
+++ src/pages/HomePage/components/AboutSection.tsx
@@ -0,0 +1,8 @@
+import { AboutPlanningPokerContent } from "../../AboutPage/AboutPage";
+import { Section } from "./Section";
+
+export const AboutSection = () => (
+    <Section>
+        <AboutPlanningPokerContent />
+    </Section>
+);
---
+++ src/pages/HomePage/components/HeroSection.tsx
@@ -0,0 +1,45 @@
+import { CreateGame } from "@/src/components/Poker/CreateGame/CreateGame";
+import { JoinGame } from "@/src/components/Poker/JoinGame/JoinGame";
+import { Section } from "@/src/pages/HomePage/components";
+import { H1, H4 } from "@/src/components/Typography";
+import { Button } from "@/src/components/ui/button";
+import { useTranslation } from "react-i18next";
+import { useMatch, useNavigate } from "react-router-dom";
+import LandingImage from './../../../images/background.jpg';
+
+export const HeroSection = () => {
+    const isJoin = !!useMatch('/join');
+    const isCreate = !!useMatch('/create');
+    const navigate = useNavigate();
+    const { t } = useTranslation();
+
+    return (<div>
+        <Section>
+            <div className='flex flex-col-reverse md:flex-row p-4 items-stretch gap-4' >
+                <div className='flex flex-1 flex-col'>
+                    <H1 className='text-left text-wrap'>{t('HomePage.heroSection.description')}</H1>
+                    <H4 className='text-left text-wrap my-6 md:my-4'>{t('HomePage.heroSection.sub')}</H4>
+                    <div className='flex flex-row gap-2 mt-auto justify-center'>
+                        <Button className='px-4 py-6' variant='secondary' onClick={() => navigate('/join')}>
+                            <H4>{t('HomePage.heroSection.joinSessionButton')}</H4>
+                        </Button>
+                        <Button className='px-4 py-6' onClick={() => navigate('/create')}>
+                            <H4>{t('HomePage.heroSection.createSessionButton')}</H4>
+                        </Button>
+                    </div>
+                </div>
+                <CreateGame open={isCreate} onClose={() => navigate('/')} />
+                <JoinGame open={isJoin} onClose={() => navigate('/')} />
+                <div className='flex-1'>
+                    <img
+                        loading='lazy'
+                        alt={t('HomePage.heroSection.title')}
+                        className='w-full h-auto rounded-lg shadow-md'
+                        src={LandingImage}
+                    />
+                </div>
+            </div>
+        </Section>
+    </div>
+    );
+};
